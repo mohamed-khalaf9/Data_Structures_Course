d82d55f17fd5f0dc677f150fd074b1880cd7f96c
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2024-07-18 20:04:50 +0300

    day20: solved med problem

--- Binary Tree/LeetcodeProblems/probelm.cpp
+++ Binary Tree/LeetcodeProblems/probelm.cpp
@@ -25,3 +25,28 @@
         return root;
         
     }
+    //
+      vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        vector<vector<int>> result;
+        auto compare = [](const tuple<int, int, int>& a, const tuple<int, int, int>& b) {
+            return get<0>(a) > get<0>(b);
+        };
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, decltype(compare)> minHeap(compare);
+
+        // Push initial pairs into the heap
+        for (int i = 0; i < nums1.size() && i < k; ++i) {
+            minHeap.emplace(nums1[i] + nums2[0], i, 0);
+        }
+
+        while (k-- > 0 && !minHeap.empty()) {
+            auto [sum, i, j] = minHeap.top();
+            minHeap.pop();
+            result.push_back({nums1[i], nums2[j]});
+            
+            if (j + 1 < nums2.size()) {
+                minHeap.emplace(nums1[i] + nums2[j + 1], i, j + 1);
+            }
+        }
+
+        return result;
+    }
